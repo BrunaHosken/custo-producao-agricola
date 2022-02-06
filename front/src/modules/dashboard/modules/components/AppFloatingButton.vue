decbd5d3eb43874ebf8fcd0f2cc0026f674ce64c
BrunaHosken <bruna16.hosken@gmail.com>
2022-02-06 13:17:39 -0300

    adicionando alerta de deletar

--- front/src/modules/dashboard/modules/components/AppFloatingButton.vue
+++ front/src/modules/dashboard/modules/components/AppFloatingButton.vue
@@ -1,34 +1,55 @@
 <template>
-  <v-speed-dial
-    v-model="fab"
-    fixed
-    bottom
-    right
-    direction="top"
-    transition="slide-y-reverse-transition"
-  >
-    <template v-slot:activator>
-      <v-btn v-model="fab" color="primary" fab>
-        <v-icon v-if="fab">mdi-close</v-icon>
-        <v-icon v-else>mdi-plus</v-icon>
+  <div>
+    <v-speed-dial
+      v-model="fab"
+      fixed
+      bottom
+      right
+      direction="top"
+      transition="slide-y-reverse-transition"
+    >
+      <template v-slot:activator>
+        <v-btn v-model="fab" fab :color="fab ? 'red accent-2' : 'primary'">
+          <v-icon v-if="fab">mdi-close</v-icon>
+          <v-icon v-else>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </template>
+
+      <v-btn
+        fab
+        small
+        color="red accent-2"
+        title="Remover"
+        :disabled="!disabled"
+        @click="deleteDialog"
+      >
+        <!-- @click="delete" -->
+        <v-icon>mdi-minus</v-icon>
       </v-btn>
-    </template>
 
-    <v-btn fab small color="red accent-2" title="Remover" :disabled="disabled">
-      <!-- @click="delete" -->
-      <v-icon>mdi-minus</v-icon>
-    </v-btn>
+      <v-btn fab small color="warning" title="Editar" :disabled="disabled">
+        <!-- @click="edit" -->
+        <v-icon>mdi-pencil-outline</v-icon>
+      </v-btn>
 
-    <v-btn fab small color="warning" title="Editar" :disabled="disabled">
-      <!-- @click="edit" -->
-      <v-icon>mdi-pencil-outline</v-icon>
-    </v-btn>
-
-    <v-btn fab small color="primary" title="Cadastrar">
-      <!-- @click="new" -->
-      <v-icon>mdi-plus</v-icon>
-    </v-btn>
-  </v-speed-dial>
+      <v-btn fab small color="success" title="Cadastrar">
+        <!-- @click="new" -->
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+    </v-speed-dial>
+    <v-dialog v-model="showDeleteDialog" max-width="300px">
+      <v-card>
+        <v-card-title>
+          <h3 class="subheading">Deseja realmente sair?</h3>
+        </v-card-title>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn text small @click="showDeleteDialog = false">Não</v-btn>
+          <v-btn text small @click="deleteItem">Sim</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
@@ -40,21 +61,31 @@ export default {
       default: true,
     },
   },
-  data: () => ({
-    fab: false,
-  }),
+  data() {
+    return {
+      fab: false,
+      showDeleteDialog: false,
+    };
+  },
 
-  // methods: {
-  //   addRecord(type) {
-  //     this.$router
-  //       .push({
-  //         name: "recordsAdd",
-  //         query: {
-  //           type,
-  //         },
-  //       })
-  //       .catch(() => {});
-  //   },
-  // },
+  methods: {
+    deleteDialog() {
+      this.showDeleteDialog = true;
+    },
+    deleteItem() {
+      console.log("deletou");
+      this.showDeleteDialog = false;
+    },
+    // addRecord(type) {
+    //   this.$router
+    //     .push({
+    //       name: "recordsAdd",
+    //       query: {
+    //         type,
+    //       },
+    //     })
+    //     .catch(() => {});
+    // },
+  },
 };
 </script>
