d1911c1c1cccedd749d3a925c8ffc68d3eeea734
BrunaHosken <bruna16.hosken@gmail.com>
2022-02-06 19:18:26 -0300

    validando campos login

--- front/src/main.js
+++ front/src/main.js
@@ -1,14 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
+import vuelidate from "./plugins/vuelidate";
+import moment from "./plugins/moment";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
+  moment,
   router,
   store,
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  vuelidate,
+  render: (h) => h(App),
+}).$mount("#app");
--- front/src/modules/auth/views/Login.vue
+++ front/src/modules/auth/views/Login.vue
@@ -32,30 +32,30 @@
                 name="name"
                 label="Nome"
                 type="text"
-              ></v-text-field>
-              <!-- :error-messages="nameErrors"
+                :error-messages="nameErrors"
                 :success="!$v.user.name.$invalid"
-                v-model.trim="$v.user.name.$model" -->
+                v-model.trim="$v.user.name.$model"
+              ></v-text-field>
               <v-text-field
                 v-model="user.email"
                 prepend-inner-icon="mdi-email"
                 name="email"
                 label="Email"
                 type="email"
-              ></v-text-field>
-              <!-- :error-messages="emailErrors"
+                :error-messages="emailErrors"
                 :success="!$v.user.email.$invalid"
-                v-model.trim="$v.user.email.$model" -->
+                v-model.trim="$v.user.email.$model"
+              ></v-text-field>
               <v-text-field
                 v-model="user.password"
                 prepend-inner-icon="mdi-lock"
                 name="password"
                 label="Senha"
                 type="password"
-              ></v-text-field>
-              <!-- :error-messages="passwordErrors"
+                :error-messages="passwordErrors"
                 :success="!$v.user.password.$invalid"
-                v-model.trim="$v.user.password.$model" -->
+                v-model.trim="$v.user.password.$model"
+              ></v-text-field>
             </v-form>
             <v-btn block depressed color="secondary" @click="newUser">
               {{ texts.button }}
@@ -73,7 +73,12 @@
 
             <v-spacer></v-spacer>
             <!-- :disabled="$v.$invalid" @click="submit"-->
-            <v-btn color="primary" large @click="submit">
+            <v-btn
+              :disabled="$v.$invalid"
+              color="primary"
+              large
+              @click="submit"
+            >
               {{ texts.toolbar }}
             </v-btn>
           </v-card-actions>
@@ -97,7 +102,7 @@
 
 //
 <script>
-// import { required, email, minLength } from "vuelidate/lib/validators";
+import { required, email, minLength } from "vuelidate/lib/validators";
 // import AuthService from "./../services/auth-service";
 // import { formatError } from "@/utils";
 
@@ -116,32 +121,32 @@ export default {
     isForgot: false,
   }),
 
-  // validations() {
-  //   const validators = {
-  //     user: {
-  //       email: {
-  //         required,
-  //         email,
-  //       },
-  //       password: {
-  //         required,
-  //         minLength: minLength(6),
-  //       },
-  //     },
-  //   };
-  //   if (this.isLogin) {
-  //     return validators;
-  //   }
-  //   return {
-  //     user: {
-  //       ...validators.user,
-  //       name: {
-  //         required,
-  //         minLength: minLength(2),
-  //       },
-  //     },
-  //   };
-  // },
+  validations() {
+    const validators = {
+      user: {
+        email: {
+          required,
+          email,
+        },
+        password: {
+          required,
+          minLength: minLength(6),
+        },
+      },
+    };
+    if (this.isLogin) {
+      return validators;
+    }
+    return {
+      user: {
+        ...validators.user,
+        name: {
+          required,
+          minLength: minLength(2),
+        },
+      },
+    };
+  },
   computed: {
     texts() {
       if (this.isLogin) return { toolbar: "Entrar", button: "Criar conta" };
@@ -149,44 +154,44 @@ export default {
         return { toolbar: "Nova Senha", button: "Já tenho uma conta" };
       return { toolbar: "Criar Conta", button: "Já tenho uma conta" };
     },
+
+    emailErrors() {
+      const errors = [];
+      const email = this.$v.user.email;
+      if (!email.$dirty) {
+        return errors;
+      }
+      !email.required && errors.push("Email é obrigatório!");
+      !email.email && errors.push("Insira um email valido!");
+      return errors;
+    },
+    nameErrors() {
+      const errors = [];
+      const name = this.$v.user.name;
+      if (!name.$dirty) {
+        return errors;
+      }
+      !name.required && errors.push("Nome é obrigatório!");
+      !name.minLength &&
+        errors.push(
+          `Insira pelo menos ${name.$params.minLength.min} caracteres!`
+        );
+      return errors;
+    },
+    passwordErrors() {
+      const errors = [];
+      const password = this.$v.user.password;
+      if (!password.$dirty) {
+        return errors;
+      }
+      !password.required && errors.push("Senha é obrigatória!");
+      !password.minLength &&
+        errors.push(
+          `Insira pelo menos ${password.$params.minLength.min} caracteres!`
+        );
+      return errors;
+    },
   },
-  //   emailErrors() {
-  //     const errors = [];
-  //     const email = this.$v.user.email;
-  //     if (!email.$dirty) {
-  //       return errors;
-  //     }
-  //     !email.required && errors.push("Email é obrigatório!");
-  //     !email.email && errors.push("Insira um email valido!");
-  //     return errors;
-  //   },
-  //   nameErrors() {
-  //     const errors = [];
-  //     const name = this.$v.user.name;
-  //     if (!name.$dirty) {
-  //       return errors;
-  //     }
-  //     !name.required && errors.push("Nome é obrigatório!");
-  //     !name.minLength &&
-  //       errors.push(
-  //         `Insira pelo menos ${name.$params.minLength.min} caracteres!`
-  //       );
-  //     return errors;
-  //   },
-  //   passwordErrors() {
-  //     const errors = [];
-  //     const password = this.$v.user.password;
-  //     if (!password.$dirty) {
-  //       return errors;
-  //     }
-  //     !password.required && errors.push("Senha é obrigatória!");
-  //     !password.minLength &&
-  //       errors.push(
-  //         `Insira pelo menos ${password.$params.minLength.min} caracteres!`
-  //       );
-  //     return errors;
-  //   },
-  // },
   methods: {
     forgotPassword() {
       this.isForgot = !this.isForgot;
