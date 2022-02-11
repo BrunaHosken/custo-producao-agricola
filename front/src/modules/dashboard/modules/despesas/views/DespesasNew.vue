23bf48a80ffa8a2fc2d65f6df2fa112c7fe62cf0
BrunaHosken <bruna16.hosken@gmail.com>
2022-02-10 23:31:05 -0300

    criacao novas despesas

--- front/src/modules/dashboard/modules/despesas/views/DespesasNew.vue
+++ front/src/modules/dashboard/modules/despesas/views/DespesasNew.vue
@@ -1,9 +1,230 @@
 <template>
-  <h1>Despesas Novo</h1>
+  <v-container fill-height>
+    <v-layout justify-center align-center>
+      <v-flex xs10>
+        <v-card class="elevation-24" outlined>
+          <v-toolbar color="primary" dark>
+            <v-list-item two-line>
+              <v-list-item-content>
+                <v-list-item-title class="text-h5 mb-1">
+                  Cadastrar uma nova Despesa
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-toolbar>
+
+          <v-card-text>
+            <v-form v-model="valid">
+              <v-container>
+                <v-row class="mt-2">
+                  <v-col cols="12" md="6">
+                    <v-dialog
+                      ref="dateDialog"
+                      :return-value.sync="form.date"
+                      v-model="showDateDialog"
+                      persistent
+                      lazy
+                      width="290px"
+                      fullwidth
+                    >
+                      <template v-slot:activator="{ on }">
+                        <v-text-field
+                          name="date"
+                          label="Data da Despesa"
+                          prepend-inner-icon="mdi-calendar"
+                          type="text"
+                          readonly
+                          :value="formattedDate"
+                          v-on="on"
+                        ></v-text-field>
+                      </template>
+
+                      <v-date-picker
+                        locale="pt-br"
+                        scrollable
+                        color="primary"
+                        v-model="dateDialogValue"
+                      >
+                        <v-spacer></v-spacer>
+                        <v-btn text @click="cancelDateDialog"> Cancelar </v-btn>
+                        <v-btn
+                          text
+                          @click="$refs.dateDialog.save(dateDialogValue)"
+                        >
+                          Ok
+                        </v-btn>
+                      </v-date-picker>
+                    </v-dialog>
+                  </v-col>
+
+                  <v-col cols="12" md="6">
+                    <v-select
+                      v-model="form.tipo"
+                      :items="items"
+                      label="Tipo de Despesa"
+                      outlined
+                    ></v-select>
+                  </v-col>
+                </v-row>
+
+                <v-row class="mt-0">
+                  <v-col cols="12" md="6">
+                    <v-text-field
+                      :error-messages="descriptionErrors"
+                      :success="!$v.form.descricao.$invalid"
+                      v-model.trim="$v.form.descricao.$model"
+                      label="Descrição da Despesa"
+                      required
+                    ></v-text-field>
+                  </v-col>
+
+                  <v-col cols="12" md="6">
+                    <v-text-field
+                      :error-messages="valueErrors"
+                      :success="!$v.form.valor.$invalid"
+                      v-model.trim="$v.form.valor.$model"
+                      label="Valor da Despesa"
+                      :value="form.valor"
+                      prefix="R$"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="secundary" class="mr-4" @click="cancel">
+              Cancelar
+            </v-btn>
+            <v-btn
+              color="orange accent-4k"
+              class="mr-4"
+              @click="showClearDialog = true"
+            >
+              Limpar
+            </v-btn>
+            <v-btn
+              color="success"
+              class="mr-4"
+              :disabled="$v.$invalid"
+              @click="save"
+            >
+              Enviar
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+        <Dialog
+          message="Deseja realmente limpar os dados?"
+          :showDialog="showClearDialog"
+          @option="option"
+        />
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
+import moment from "moment";
+import { required, minLength, minValue } from "vuelidate/lib/validators";
+
+import Dialog from "./../../../components/Dialog.vue";
 export default {
   name: "DespesasNew",
+  components: {
+    Dialog,
+  },
+  data() {
+    return {
+      valid: false,
+      items: ["Fixo", "Variavel"],
+      form: {
+        date: moment().format("YYYY-MM-DD"),
+        descricao: "",
+        valor: 0,
+        tipo: "Fixo",
+      },
+
+      showDateDialog: false,
+      dateDialogValue: moment().format("YYYY-MM-DD"),
+      showClearDialog: false,
+    };
+  },
+  validations() {
+    return {
+      form: {
+        descricao: {
+          required,
+          minLength: minLength(2),
+        },
+        valor: {
+          required,
+          minValue: minValue(0.0000001),
+        },
+      },
+    };
+  },
+  computed: {
+    formattedDate() {
+      return moment(this.form.date).format("DD/MM/YYYY");
+    },
+    greaterThanZero() {
+      return this.form.valor > 0;
+    },
+    descriptionErrors() {
+      const errors = [];
+      const description = this.$v.form.descricao;
+      if (!description.$dirty) {
+        return errors;
+      }
+      !description.required && errors.push("Descrição é obrigatória!");
+      !description.minLength &&
+        errors.push(
+          `Insira pelo menos ${description.$params.minLength.min} caracteres!`
+        );
+      return errors;
+    },
+    valueErrors() {
+      const errors = [];
+      const value = this.$v.form.valor;
+      if (!value.$dirty) {
+        return errors;
+      }
+      !value.required && errors.push("Valor da despesa é obrigatória!");
+      !value.minValue && errors.push(`Insira um valor acima de 0`);
+      return errors;
+    },
+  },
+  methods: {
+    option(data) {
+      if (data == "nao") {
+        this.showClearDialog = false;
+      } else {
+        this.clean();
+        this.showClearDialog = false;
+      }
+    },
+    cancelDateDialog() {
+      this.showDateDialog = false;
+      this.dateDialogValue = this.form.date;
+    },
+    cancel() {
+      this.$router.go(-1);
+    },
+    clean() {
+      this.form = {
+        date: moment().format("YYYY-MM-DD"),
+        tipo: "Fixo",
+        descricao: "",
+        valor: 0,
+      };
+    },
+    save() {
+      console.log(this.form);
+      // this.$v.$reset();
+      // this.clear();
+      this.$router.go(-1);
+    },
+  },
 };
 </script>
