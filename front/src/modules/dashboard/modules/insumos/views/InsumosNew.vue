8e07782a9ca61939ff6c2668f7405381d3618b40
BrunaHosken <bruna16.hosken@gmail.com>
2022-02-13 19:59:12 -0300

    tela insumos - unidades

--- front/src/modules/dashboard/modules/insumos/views/InsumosNew.vue
+++ front/src/modules/dashboard/modules/insumos/views/InsumosNew.vue
@@ -120,7 +120,7 @@ export default {
         "Toneladas",
         "Quilograma/Litro",
         "Maço",
-        "Unitário",
+        "Quilograma",
         "Litros",
         "Quilowatt",
       ],
