00b2433ba284dc66eb258efc9af69fc916541f71
BrunaHosken <bruna16.hosken@gmail.com>
2022-04-17 23:16:07 -0300

    basico das querys

--- back/src/resolvers/Query.js
+++ back/src/resolvers/Query.js
@@ -4,6 +4,92 @@ function agricultor(_, args, ctx, info) {
   return ctx.db.query.agricultor({ where: { id: agricultorId } }, info);
 }
 
+function tipoDespesas(_, args, ctx, info) {
+  return ctx.db.query.tipoDespesas(info);
+}
+
+function tipoInsumoes(_, args, ctx, info) {
+  return ctx.db.query.tipoInsumoes({ orderBy: "NomeTipo_ASC" }, info);
+}
+
+function servicoes(_, args, ctx, info) {
+  return ctx.db.query.servicoes({ orderBy: "DescrServico_ASC" }, info);
+}
+function clientes(_, args, ctx, info) {
+  return ctx.db.query.clientes({ orderBy: "NomeCliente_ASC" }, info);
+}
+function culturas(_, args, ctx, info) {
+  return ctx.db.query.culturas({ orderBy: "DescrCultura_ASC" }, info);
+}
+function vendas(_, args, ctx, info) {
+  return ctx.db.query.vendas({ orderBy: "Data_ASC" }, info);
+}
+function despesaRealizadas(_, args, ctx, info) {
+  const agricultorId = getAgricultorId(ctx);
+  return ctx.db.query.despesaRealizadas(
+    {
+      where: {
+        Agricultor: {
+          id: agricultorId,
+        },
+      },
+
+      orderBy: "DescrDetalhada_ASC",
+    },
+    info
+  );
+}
+function culturaDesenvolvidas(_, args, ctx, info) {
+  const agricultorId = getAgricultorId(ctx);
+  return ctx.db.query.culturaDesenvolvidas(
+    {
+      where: {
+        Agricultor: {
+          id: agricultorId,
+        },
+      },
+
+      orderBy: "DataInicio_ASC",
+    },
+    info
+  );
+}
+function insumoes(_, args, ctx, info) {
+  return ctx.db.query.insumoes({ orderBy: "DescrInsumo_ASC" }, info);
+}
+function vendaItems(_, args, ctx, info) {
+  return ctx.db.query.vendaItems({ orderBy: "Qtd_ASC" }, info);
+}
+function culturaEtapas(_, args, ctx, info) {
+  return ctx.db.query.culturaEtapas({ orderBy: "DescrEtapa_ASC" }, info);
+}
+function usoInsumoReals(_, args, ctx, info) {
+  return ctx.db.query.usoInsumoReals({ orderBy: "Data_ASC" }, info);
+}
+function usoInsumoPrevistoes(_, args, ctx, info) {
+  return ctx.db.query.usoInsumoPrevistoes({ orderBy: "Qtd_ASC" }, info);
+}
+function servicoPrestadoes(_, args, ctx, info) {
+  return ctx.db.query.servicoPrestadoes({ orderBy: "Data_ASC" }, info);
+}
+function servicoPrevistoes(_, args, ctx, info) {
+  return ctx.db.query.servicoPrevistoes({ orderBy: "DiasHomem_ASC" }, info);
+}
 module.exports = {
   agricultor,
+  tipoDespesas,
+  tipoInsumoes,
+  servicoes,
+  clientes,
+  culturas,
+  vendas,
+  despesaRealizadas,
+  culturaDesenvolvidas,
+  insumoes,
+  vendaItems,
+  culturaEtapas,
+  usoInsumoReals,
+  usoInsumoPrevistoes,
+  servicoPrestadoes,
+  servicoPrevistoes,
 };
